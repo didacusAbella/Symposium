b1575d0f02cba18fcf72bb78856af38efeb4bb55
GMadness97 <madonna.giuseppe@outlook.it>
2018-12-19 16:15:12 +0100

    AdminManager Completed
    AdminManager, Model Completed

--- src/main/java/it/blackhat/symposium/managers/AdminModelManager.java
+++ src/main/java/it/blackhat/symposium/managers/AdminModelManager.java
@@ -4,10 +4,14 @@ import it.blackhat.symposium.models.Admin;
 import it.blackhat.symposium.models.AdminModel;
 import static it.blackhat.symposium.queries.AdminQuery.*;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
+import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
@@ -18,42 +22,31 @@ import org.apache.commons.dbutils.handlers.BeanListHandler;
 public class AdminModelManager extends ConnectionManager implements AdminManager {
 
     @Override
-    public Optional<Admin> findAdmin(String username, String password)
-            throws SQLException {
-        QueryRunner findQuery = new QueryRunner(this.dataSource);
-        Admin foundAdmin = findQuery.query(FIND_ADMIN,
-                new BeanHandler<>(AdminModel.class), username, password);
-        return Optional.ofNullable(foundAdmin);
+	public Optional<Admin> findAdminBy(String username, String password) throws SQLException {
+        QueryRunner run = new QueryRunner(this.dataSource);       
+        BeanHandler<Admin> h = new BeanHandler<>(Admin.class);
+        Admin result = run.query("" , h);
+        return Optional.ofNullable(result);
     }
 
     @Override
-    public Optional<Admin> createAdmin(Admin admin) throws SQLException {
-        QueryRunner insertQuery = new QueryRunner(this.dataSource);
-        Admin createdAdmin = insertQuery.insert(ADD_ADMIN, new BeanHandler<>(AdminModel.class), 
-                admin.getUsername(), admin.getPassword());
-        return Optional.ofNullable(createdAdmin);
+    public boolean banUser(String email) throws SQLException {
+        QueryRunner run = new QueryRunner(this.dataSource);
+        int rowMod = run.update("");
+        if (rowMod == 1) {
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public int updateAdmin(Admin admin) throws SQLException {
-        QueryRunner updateQuery = new QueryRunner(this.dataSource);
-        int updatedAdmins = updateQuery.update(UPDATE_ADMIN, admin.getPassword(), 
-                admin.getUsername());
-        return updatedAdmins;
+    public Optional<String> genReport(String adminUsername) throws SQLException {
+        QueryRunner run = new QueryRunner(this.dataSource);
+        BeanHandler<String> h = new BeanHandler<>(String.class);
+        String result = run.query("", h);
+        return Optional.ofNullable(result);
     }
-
-    @Override
-    public int deleteAdmin(Admin admin) throws SQLException {
-        QueryRunner deleteQuery = new QueryRunner(this.dataSource);
-        int deletedAdmins = deleteQuery.update(DELETE_ADMIN, admin.getUsername());
-        return deletedAdmins;
-    }
-
-    @Override
-    public List<Admin> findAllAdmins() throws SQLException {
-        QueryRunner adminsQuery = new QueryRunner(this.dataSource);
-        List<Admin> admins = adminsQuery.query(ALL_ADMINS, new BeanListHandler<>(AdminModel.class));
-        return admins;
-    }
-
+    
+    
+    
 }
