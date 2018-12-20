feaba9c2b165b2d7d9d77e43f12507d3815f6b56
GMadness97 <madonna.giuseppe@outlook.it>
2018-12-20 17:36:56 +0100

    Removed UnUsedImport
    Removed unused import in SignInAdminAction

--- src/main/java/it/blackhat/symposium/actions/admin/SigninAdminAction.java
+++ src/main/java/it/blackhat/symposium/actions/admin/SigninAdminAction.java
@@ -1,11 +1,6 @@
 package it.blackhat.symposium.actions.admin;
 
 import it.blackhat.symposium.actions.Action;
-import it.blackhat.symposium.managers.AdminManager;
-import it.blackhat.symposium.managers.AdminModelManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,8 +9,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author didacus
  */
 public class SigninAdminAction implements Action {
-
-    private final AdminManager adminManager = new AdminModelManager();
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
