1b59c62c6cb14582ff0cc9fc3f508d5b5caf3bae
Andrea Raiola <a.raiola14@studenti.unisa.it>
2018-12-22 03:52:01 +0100

    Added CompositeAction.java

---
+++ src/main/java/it/blackhat/symposium/actions/CompositeAction.java
@@ -0,0 +1,29 @@
+package it.blackhat.symposium.actions;
+
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Composite Action
+ *
+ * @version 0.1
+ * @author 2Deimos
+ */
+public class CompositeAction implements Action {
+    
+    private final ArrayList<Action> actions;
+
+    public CompositeAction(ArrayList<Action> actions) {
+        this.actions = actions;
+    }
+    
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) {
+        for (Action action : actions) {
+            action.execute(req, res);
+        }
+        return "";
+    }
+
+}
