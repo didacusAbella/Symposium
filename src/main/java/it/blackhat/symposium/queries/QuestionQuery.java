cd6fbf79caa5cf8614c57baf7d421b9a9884b683
jackmann <saxmilan97@live.it>
2018-12-30 11:43:05 +0100

    added classes
    added tagQuery, QuestionManager,QuestionModelManager

--- src/main/java/it/blackhat/symposium/queries/QuestionQuery.java
+++ src/main/java/it/blackhat/symposium/queries/QuestionQuery.java
@@ -11,10 +11,11 @@ public final class QuestionQuery {
     	
     }
 
-    public static final String INSERT_QUESTION = "INSERT INTO question(content)" 
-			+ " VALUES(?)";
+    public static final String INSERT_QUESTION = "INSERT INTO question(content, title)" 
+			+ " VALUES(? , ?)";
+   
     public static final String DELETE_QUESTION = "DELETE FROM question WHERE email = ?";
-    public static final String CHANGE_TAG = "UPDATE tag SET name= ? WHERE question.id= ?";
+    
     public static final String QUESTION_REPORT = "UPDATE question SET numreports = numreports+1";
     public static final String RESEARCH_BY_WORDS = "SELECT content FROM question"
     		+ " WHERE content LIKE '%?%'";
@@ -24,6 +25,12 @@ public final class QuestionQuery {
     public static final String FAVORITES = "INSERT INTO favorites VALUES ( ?, ?)";
     public static final String LASTEDIT = "SELECT content FROM question\r\n" 
     		+ "ORDER BY lastmod DESC";
-    public static final String MODIFY_TAG = "UPDATE tag SET name=?";
+  
     
 }
+
+
+
+
+
+
