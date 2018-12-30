f73ee0914afbaa40b6ca3e2c84dbc79059f3d103
Unknown <maffettone@live.it>
2018-12-30 17:27:51 +0100

    Revert "Adding delete answer for both, users and admin. "
    This reverts commit d4eea5d92a161ea93c5c33d18f23db3104705031.

--- src/main/java/it/blackhat/symposium/actions/answer/DeleteAnswerAction.java
+++ src/main/java/it/blackhat/symposium/actions/answer/DeleteAnswerAction.java
@@ -1,51 +1,19 @@
 package it.blackhat.symposium.actions.answer;
 
-/*
-import it.blackhat.symposium.actions.Action;
-import it.blackhat.symposium.actions.question.DeleteQuestionAction;
-import it.blackhat.symposium.managers.QuestionManager;
-import it.blackhat.symposium.managers.QuestionModelManager;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
+import it.blackhat.symposium.actions.Action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.SQLException;
 
-/*
+/**
  * Describes the delete answer action by the user
  * 
- * @author Angelo Maffettone
+ * @author 2Deimos
  */
-/*public class DeleteAnswerAction implements Action {
-   /* private final AnswerManager answerManager;
-    private final Log deleteAnswerLog = LogFactory.getLog(DeleteAnswerAction.class);
+public class DeleteAnswerAction implements Action {
 
-
-    /**
-     * Initializes a Answer Manager
-     */
-    /*public DeleteAnswerActionAction() {
-        answerManager = new AnswerModelManager();
-    }
-
-
-    /*@Override
+    @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
-      try {
-            String idAnswer = req.getParameter("id");
-            int idAnswerInt = Integer.parseInt(idAnswer);
-
-
-            answerManager.deleteAnswer(idAnswerInt);
-
-            return "index.jsp";
-
-        } catch (SQLException e) {
-            deleteAnswerLog.error("Errore interno", e);
-            return "error500.jsp";
-        }
+    	return "";
     }
-
 }
-*/
