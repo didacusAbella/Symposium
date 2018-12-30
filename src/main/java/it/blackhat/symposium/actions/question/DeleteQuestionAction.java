4cb942bf0dcfca83079a7c3b3bb6cf1651eb7b8f
Andrea Raiola <a.raiola14@studenti.unisa.it>
2018-12-30 11:01:52 +0100

    New composite action detected

--- src/main/java/it/blackhat/symposium/actions/question/DeleteQuestionAction.java
+++ src/main/java/it/blackhat/symposium/actions/question/DeleteQuestionAction.java
@@ -1,6 +1,8 @@
 package it.blackhat.symposium.actions.question;
 
 import it.blackhat.symposium.actions.Action;
+import it.blackhat.symposium.actions.CompositeAction;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
  * Describes the delete question action by user
  * @author 2Deimos
  */
-public class DeleteQuestionAction implements Action {
+public class DeleteQuestionAction extends CompositeAction {
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
