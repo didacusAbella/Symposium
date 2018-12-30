9f49720f3078563103de6fa0a20d9525d2abfd67
jackmann <saxmilan97@live.it>
2018-12-30 16:05:57 +0100

    ignore for the moment
    added answermanager and answerModelManager, and fixed stuff, but ignore for the moment

--- src/main/java/it/blackhat/symposium/managers/QuestionModelManager.java
+++ src/main/java/it/blackhat/symposium/managers/QuestionModelManager.java
@@ -63,7 +63,7 @@ public class QuestionModelManager extends ConnectionManager implements QuestionM
     @Override
 	public int questionReport(Question question) throws SQLException {
     	QueryRunner run = new QueryRunner(this.dataSource);
-    	int upd = run.update(QUESTION_REPORT , question.getNumSegn());
+    	int upd = run.update(QUESTION_REPORT , question.getNumReports());
     	return upd;
     }
 
