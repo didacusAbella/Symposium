97f0e1e6053d49b5b99b0ba2de025bb801abf4eb
Unknown <maffettone@live.it>
2018-12-30 19:16:48 +0100

    Adding InsertAnswerAction, AnswerManager, AnswerModelManager. Adding a BEST_ANSWER query in AnswerQuery

--- src/main/java/it/blackhat/symposium/actions/answer/InsertAnswerAction.java
+++ src/main/java/it/blackhat/symposium/actions/answer/InsertAnswerAction.java
@@ -2,8 +2,19 @@ package it.blackhat.symposium.actions.answer;
 
 
 import it.blackhat.symposium.actions.Action;
+import it.blackhat.symposium.managers.AnswerManager;
+import it.blackhat.symposium.managers.AnswerModelManager;
+import it.blackhat.symposium.models.Answer;
+import it.blackhat.symposium.models.AnswerModel;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+
 
 /**
  * Describes the insert answer action by the user
@@ -11,9 +22,33 @@ import javax.servlet.http.HttpServletResponse;
  * @author 2Deimos
  */
 public class InsertAnswerAction implements Action {
+    private final AnswerManager answerManager;
+    private final Log addAnswerLog = LogFactory.getLog(InsertAnswerAction.class);
+
+    /**
+     * Initializes a Answer Manager
+     */
+    public InsertAnswerAction() {
+        answerManager = new AnswerModelManager();
+    }
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
-    	return "";
+        try {
+            Answer answer = new AnswerModel();
+            BeanUtils.populate(answer, req.getParameterMap());
+            answerManager.insertAnswer(answer);
+
+            return "/index.jsp";
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return "/error500.jsp";
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            return "/error500.jsp";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "/error500.jsp";
     }
 }
