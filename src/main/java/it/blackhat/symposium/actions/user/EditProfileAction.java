8a1f2af1a29b7326a8ca27db9aba56acbb625fae
Przemyslaw Szopian <p.szopian@studenti.unisa.it>
2019-01-02 11:06:06 +0100

    EditProfileAction fix.

--- src/main/java/it/blackhat/symposium/actions/user/EditProfileAction.java
+++ src/main/java/it/blackhat/symposium/actions/user/EditProfileAction.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package it.blackhat.symposium.actions.user;
 
 import it.blackhat.symposium.actions.Action;
