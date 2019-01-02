df313aed36fff733677ca9a109adb279198f7d97
Andrea Raiola <a.raiola14@studenti.unisa.it>
2019-01-02 20:00:20 +0100

    Named web services, fixed comments, fixed signup and signin pages

--- src/main/java/it/blackhat/symposium/filters/AdminLogInFilter.java
+++ src/main/java/it/blackhat/symposium/filters/AdminLogInFilter.java
@@ -1,24 +1,20 @@
 package it.blackhat.symposium.filters;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
- * 
+ * Admin Filter
  * @author 2Deimos
+ * @author GMadness97
  * 
  */
 
-@WebFilter("/admin/*")
+@WebFilter(filterName = "AdminLogInFilter", urlPatterns = "/admin/*")
 public class AdminLogInFilter implements Filter {
     
     @Override
