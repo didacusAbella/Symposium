7a3fe7d79274f2d7a60588f6a9bafae2e8a3df75
Carminantonio Parrilli <carmine.parrilli@gmail.com>
2019-01-03 11:26:48 +0100

    Some additions
    1. Added a method incrementSignedUpUsers to the interface StatsManager
    2. Added the method getNumberUsers() to the class StatsModelManager
    3. Added a query to StatsQuery

--- src/main/java/it/blackhat/symposium/managers/StatsManager.java
+++ src/main/java/it/blackhat/symposium/managers/StatsManager.java
@@ -7,6 +7,7 @@ import java.util.Date;
 //import it.blackhat.symposium.models.Stats;
 
 /**
+ * An interface for the manager of the Stats
  * @author Parrilli Carminantonio
  *
  */
@@ -14,8 +15,14 @@ public interface StatsManager {
     /**
      * Find the stats of the site given a year
      * @param year the year we want the stats to come from
-     * @return the stats of a given year
+     * @return the numer of rows updated
      * @throws SQLException if db errors occurred
      */
     int incrementSignedUpUsers(Date year) throws SQLException;
+    /**
+     * Return the number of users in the database
+     * @return the number of users in the database
+     * @throws SQLException if db errors occurred
+     */
+    int getNumberUsers() throws SQLException;
 }
