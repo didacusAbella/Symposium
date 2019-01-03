98c1d0d3de85b9a6ef1ad270e4044783813f3960
jackmann <saxmilan97@live.it>
2019-01-03 16:14:45 +0100

    IGNORE

--- src/main/java/it/blackhat/symposium/queries/QuestionQuery.java
+++ src/main/java/it/blackhat/symposium/queries/QuestionQuery.java
@@ -1,22 +1,23 @@
 package it.blackhat.symposium.queries;
 
 public final class QuestionQuery {
+    public static final String INSERT_QUESTION = "INSERT INTO question(content, title)"
+            + " VALUES(? , ?)";
     public static final String DELETE_QUESTION = "DELETE FROM question WHERE id = ?";
+    public static final String RESEARCH_BY_WORDS = "SELECT * FROM question"
+            + " WHERE content LIKE '%?%'";
 
-    public static final String INSERT_QUESTION = "INSERT INTO question(content, title)" 
-			+ " VALUES(? , ?)";
     public static final String QUESTION_REPORT = "UPDATE question SET numReports = numReports+1"
             + " WHERE id=?";
-    public static final String RESEARCH_BY_WORDS = "SELECT * FROM question"
-    		+ " WHERE content LIKE '%?%'";
-    public static final String RESEARCH_BY_TAG = "SELECT content\r\n"
-            + "FROM question,tag, contains"
-    		+ "WHERE question.id=contains.questionId && tag.id=contains.tagId && ?= tag.nome";
-    public static final String LASTEDIT = "SELECT content FROM question\r\n"
-            + " ORDER BY lastmod DESC";
+    public static final String RESEARCH_BY_TAG = "SELECT question.id ,content,lastUpdate,creationDate,numReports,userFk,title"
+            + " FROM question,tag, questiontag "
+            + " WHERE question.id = questiontag.questionId AND tag.id = questiontag.tagId AND ? = tag.name";
     public static final String FAVORITES = "INSERT INTO favorites VALUES ( ?, ?)";
     public static final String TAKE__ALL_QUESTIONS = "SELECT * FROM question";
+    public static final String LASTEDIT = "SELECT content FROM question\r\n"
+            + " ORDER BY lastmod DESC";
     public static final String TAKE_QUESTION = "SELECT * FROM question WHERE id=?";
+
 
     /**
      * This class specifies queries about questions for the system.
@@ -27,11 +28,4 @@ public final class QuestionQuery {
     private QuestionQuery() {
 
     }
-
 }
-
-
-
-
-
-
