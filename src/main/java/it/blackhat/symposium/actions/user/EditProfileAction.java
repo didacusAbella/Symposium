27eb0c935985885fa4a61ef493f895d398df8c49
Andrea Raiola <a.raiola14@studenti.unisa.it>
2019-01-03 16:01:14 +0100

    Fixed checkstyle

--- src/main/java/it/blackhat/symposium/actions/user/EditProfileAction.java
+++ src/main/java/it/blackhat/symposium/actions/user/EditProfileAction.java
@@ -14,6 +14,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 /**
+ * Describes edit profile action
  * @author Przemyslaw Szopian
  */
 public class EditProfileAction implements Action {
