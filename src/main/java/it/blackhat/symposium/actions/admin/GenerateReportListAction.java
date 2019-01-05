61813f5877e4c6fa2b79893abe635b71ff2815dc
Andrea Raiola <a.raiola14@studenti.unisa.it>
2019-01-05 18:34:45 +0100

    Added ReportQuestionAction feature

--- src/main/java/it/blackhat/symposium/actions/admin/GenerateReportListAction.java
+++ src/main/java/it/blackhat/symposium/actions/admin/GenerateReportListAction.java
@@ -1,16 +1,15 @@
 package it.blackhat.symposium.actions.admin;
 
+import it.blackhat.symposium.actions.Action;
+import it.blackhat.symposium.managers.ReportManager;
+import it.blackhat.symposium.managers.ReportModelManager;
+import it.blackhat.symposium.models.Report;
 import java.sql.SQLException;
-
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.util.List;
-
-import it.blackhat.symposium.actions.Action;
 /**
  * The class return a list of users for an admin
  * @author Parrilli Carminantonio
@@ -30,12 +29,12 @@ public class GenerateReportListAction implements Action {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
         try {
-            List<Report> listReports = user.retrieveUsers();
-            req.setAttribute("listUsers", listUsers);
-            return "/userList.jsp";
+            List<Report> listReports = reportManager.retrieveReport(0);
+            req.setAttribute("listReports", listReports);
+            return "/reportList.jsp";
         } catch (SQLException e) {
             showUserActionLog.error("Errore Interno SQL", e);
-            return "/error400.jsp";
+            return "/error500.jsp";
         }
     }
 
