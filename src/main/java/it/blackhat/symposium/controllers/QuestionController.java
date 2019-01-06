9b3f74259f59f9c4e0ba4e7699c6a5db9a01ff51
didacusabella <diequito93@gmail.com>
2019-01-06 15:55:41 +0100

    fix showallQuestions

--- src/main/java/it/blackhat/symposium/controllers/QuestionController.java
+++ src/main/java/it/blackhat/symposium/controllers/QuestionController.java
@@ -36,9 +36,6 @@ public class QuestionController extends DispatcherController {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException, InvalidActionException {
         String action = request.getParameter("action");
-        if(action==null){
-            action="showQuestions";
-        }
         Action questionAction = this.questionFactory.createAction(action);
         String page = questionAction.execute(request, response);
         this.dispatch(request, response, page);
