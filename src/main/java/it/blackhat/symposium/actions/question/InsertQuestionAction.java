2b06b2b384ce2b86c4b2e7f195aa33e524cbf0e3
Andrea Raiola <a.raiola14@studenti.unisa.it>
2019-01-06 16:19:10 +0100

    Added BeanValidators

--- src/main/java/it/blackhat/symposium/actions/question/InsertQuestionAction.java
+++ src/main/java/it/blackhat/symposium/actions/question/InsertQuestionAction.java
@@ -3,6 +3,7 @@ package it.blackhat.symposium.actions.question;
 
 import it.blackhat.symposium.actions.Action;
 import it.blackhat.symposium.actions.CompositeAction;
+import it.blackhat.symposium.helpers.BeanValidator;
 import it.blackhat.symposium.helpers.TagExtractor;
 import it.blackhat.symposium.managers.QuestionManager;
 import it.blackhat.symposium.managers.QuestionModelManager;
@@ -32,6 +33,7 @@ public class InsertQuestionAction extends CompositeAction {
 
     /**
      * Create a new insert action
+     *
      * @param actions other actions to execute
      */
     public InsertQuestionAction(Action... actions) {
@@ -44,17 +46,21 @@ public class InsertQuestionAction extends CompositeAction {
         try {
             Question newQuestion = new QuestionModel();
             BeanUtils.populate(newQuestion, req.getParameterMap());
-            newQuestion.setCreationDate(new Date(Calendar.getInstance().getTime().getTime()));
-            newQuestion.setLastUpdate(new Date(Calendar.getInstance().getTime().getTime()));
-            UserModel currentUser = (UserModel) req.getSession().getAttribute("user");
-            newQuestion.setUserFk(currentUser.getEmail());
-            int idquestion = this.questionManager.insertQuestion(newQuestion);
-            super.execute(req, res);
-            String[] tagList = TagExtractor.extractTag(req);
-            for (String tag : tagList) {
-                this.questionManager.insertQuestionTag(idquestion, tag);
+            if (BeanValidator.validateBean(newQuestion)) {
+                newQuestion.setCreationDate(new Date(Calendar.getInstance().getTime().getTime()));
+                newQuestion.setLastUpdate(new Date(Calendar.getInstance().getTime().getTime()));
+                UserModel currentUser = (UserModel) req.getSession().getAttribute("user");
+                newQuestion.setUserFk(currentUser.getEmail());
+                int idQuestion = this.questionManager.insertQuestion(newQuestion);
+                super.execute(req, res);
+                String[] tagList = TagExtractor.extractTag(req);
+                for (String tag : tagList) {
+                    this.questionManager.insertQuestionTag(idQuestion, tag);
+                }
+                return "/index.jsp";
+            } else {
+                return "/error400.jsp";
             }
-            return "/index.jsp";
         } catch (IllegalAccessException e) {
             this.insertQustionLog.error("Accesso Illegale", e);
             return "/error500.jsp";
