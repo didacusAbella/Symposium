eb3e9a57c401cf08596b652cb69525a89ed5ac3f
Andrea Raiola <a.raiola14@studenti.unisa.it>
2019-01-07 16:11:38 +0100

    Added QuestionModelManager test and BeanValidator prototype test.

---
+++ src/test/java/it/blackhat/symposium/unit/BeanValidatorTest.java
@@ -0,0 +1,32 @@
+package it.blackhat.symposium.unit;
+
+import it.blackhat.symposium.helpers.BeanValidator;
+import it.blackhat.symposium.models.Question;
+import it.blackhat.symposium.models.QuestionModel;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
+
+public class BeanValidatorTest {
+    Question question;
+
+    @Test
+    public void testValidateBeanValid() {
+        question = new QuestionModel("Test bellissimo", "test della validazione in corso",
+                "a.raiola14@studenti.unisa.it", new Date(Calendar.getInstance().getTime().getTime()),
+                new Date(Calendar.getInstance().getTime().getTime()), 10, 1);
+        Assert.assertEquals(true, BeanValidator.validateBean(question));
+    }
+
+    @Test
+    public void testValidateBeanInvalid() {
+        question = new QuestionModel(null, "test della validazione in corso",
+                "a.raiola14@studenti.unisa.it", null,
+                null, 10, 1);
+        assertEquals(false, BeanValidator.validateBean(question));
+    }
+}
