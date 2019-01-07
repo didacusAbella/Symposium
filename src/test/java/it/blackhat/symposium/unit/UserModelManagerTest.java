f81063070cf3b559ae2612b3b66b0f8b4e6f2c33
GMadness97 <madonna.giuseppe@outlook.it>
2019-01-07 14:30:34 +0100

    Added TestUser
    Added UserModelTest
    Added UserModelMangerTest
    Added UserActionFactory

---
+++ src/test/java/it/blackhat/symposium/unit/UserModelManagerTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.blackhat.symposium.unit;
+
+import it.blackhat.symposium.managers.UserManager;
+import it.blackhat.symposium.managers.UserModelManager;
+import java.sql.SQLException;
+import org.junit.Assert;
+
+
+/**
+ *
+ * @author acer
+ */
+public class UserModelManagerTest {
+    
+    public UserModelManagerTest() throws SQLException{
+        UserManager testCostructorUser = new UserModelManager();
+        Assert.assertTrue(testCostructorUser instanceof UserModelManager);
+    }  
+}
