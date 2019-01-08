90fc4955a7bf08e426a3bc3844dd7d6edc8859f1
didacusabella <diequito93@gmail.com>
2019-01-08 11:13:11 +0100

    add dump db

--- src/test/java/it/blackhat/symposium/integration/IntegrationTestSuite.java
+++ src/test/java/it/blackhat/symposium/integration/IntegrationTestSuite.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.SQLException;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.h2.tools.DeleteDbFiles;
 import org.h2.tools.RunScript;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -24,7 +26,7 @@ public class IntegrationTestSuite {
     public static BasicDataSource mockDbDatasource;
     
     @BeforeClass
-    public static void beforeClass() throws SQLException, FileNotFoundException {
+    public static void setupDb() throws SQLException, FileNotFoundException {
         mockDbDatasource = new BasicDataSource();
         mockDbDatasource.setDriverClassName("org.h2.Driver");
         mockDbDatasource.setUrl("jdbc:h2:it./symposiumTest:symposiumdb1;MODE=MYSQL;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1");
@@ -33,6 +35,13 @@ public class IntegrationTestSuite {
         RunScript.execute(mockDbDatasource.getConnection(), 
                 new FileReader(new File(IntegrationTestSuite.class.getResource("/dump.sql")
                         .getFile())));
+    }
+    
+    
+    @AfterClass
+    public static void clenupDb() throws SQLException{
+        mockDbDatasource.getConnection()
+                .createStatement().execute("drop all objects delete files");
     }
     
 }
