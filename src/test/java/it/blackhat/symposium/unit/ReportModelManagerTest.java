f881482e176f29df8f6607b8b3ba929eb24d67ec
Rayman <g.demaio24@studenti.unisa.it>
2019-01-08 16:23:54 +0100

    Added ReportModelManager and ReportModel tests

--- src/test/java/it/blackhat/symposium/unit/ReportModelManagerTest.java
+++ src/test/java/it/blackhat/symposium/unit/ReportModelManagerTest.java
@@ -1,14 +1,23 @@
 package it.blackhat.symposium.unit;
 
 import it.blackhat.symposium.managers.ReportModelManager;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.sql.SQLException;
+import javax.sql.DataSource;
+
+/**
+ * @Rayman
+ */
 
 public class ReportModelManagerTest {
-
-    public ReportModelManagerTest() throws SQLException {
-        ReportModelManager testConstrucorReport = new ReportModelManager();
-        Assert.assertTrue(testConstrucorReport instanceof ReportModelManager);
+@Test
+public void constractorEmpty() {
+    DataSource ds = Mockito.mock(BasicDataSource.class);
+    ReportModelManager testConstrucorReport = new ReportModelManager(ds);
+    Assert.assertTrue(testConstrucorReport instanceof ReportModelManager);
     }
+
 }
--- src/test/java/it/blackhat/symposium/unit/ReportModelTest.java
+++ src/test/java/it/blackhat/symposium/unit/ReportModelTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
+ *
  * @author Rayman
  */
 
