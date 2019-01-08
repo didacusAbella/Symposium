8668a63aa389e44ff16ce6781b1d6e1d23972d11
Domenico <dtropeano@hotmail.it>
2019-01-08 13:35:15 +0100

    Example System Test

--- src/test/java/it/blackhat/symposium/system/SystemTestSuite.java
+++ src/test/java/it/blackhat/symposium/system/SystemTestSuite.java
@@ -1,16 +1,30 @@
 package it.blackhat.symposium.system;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 
 /**
- *
  * @author didacus
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    TestSystem.class
+
 })
 public class SystemTestSuite {
-  
+    public static WebDriver seleniumDriver;
+
+    @BeforeClass
+    public static void InitDriver() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("headless");
+        ChromeDriverManager.getInstance(CHROME).setup();
+        SystemTestSuite.seleniumDriver = new ChromeDriver(chromeOptions);
+    }
 }
