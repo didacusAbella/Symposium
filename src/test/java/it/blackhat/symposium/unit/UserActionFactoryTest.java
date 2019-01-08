86aa516891ca906a122033dd49047e5a70cf3a5e
GMadness97 <madonna.giuseppe@outlook.it>
2019-01-08 10:52:25 +0100

    MicroFix

--- src/test/java/it/blackhat/symposium/unit/UserActionFactoryTest.java
+++ src/test/java/it/blackhat/symposium/unit/UserActionFactoryTest.java
@@ -14,7 +14,6 @@ import it.blackhat.symposium.actions.user.SignoutAction;
 import it.blackhat.symposium.actions.user.UserActionFactory;
 import it.blackhat.symposium.helpers.InvalidActionException;
 import org.junit.Assert;
-import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -22,46 +21,45 @@ import org.junit.Test;
  * @author Giuseppe Madonna
  */
 public class UserActionFactoryTest {
-   
 
     @Test
     public void createActionSignOutTest() throws InvalidActionException {
-            Action actionSignOut;
-            actionSignOut = (Action) new UserActionFactory().createAction("signOut");
-            Assert.assertTrue(actionSignOut instanceof SignoutAction);
+        Action actionSignOut;
+        actionSignOut = (Action) new UserActionFactory().createAction("signOut");
+        Assert.assertTrue(actionSignOut instanceof SignoutAction);
     }
-    
+
     @Test
     public void createActionShowProfileTest() throws InvalidActionException {
-            Action actionShowProfile;
-            actionShowProfile = (Action) new UserActionFactory().createAction("showProfile");
-            Assert.assertTrue(actionShowProfile instanceof ShowProfileAction);
+        Action actionShowProfile;
+        actionShowProfile = (Action) new UserActionFactory().createAction("showProfile");
+        Assert.assertTrue(actionShowProfile instanceof ShowProfileAction);
     }
-    
+
     @Test
     public void createActionEditProfileTest() throws InvalidActionException {
-            Action actionEditProfile;
-            actionEditProfile = (Action) new UserActionFactory().createAction("editProfile");
-            Assert.assertTrue(actionEditProfile instanceof EditProfileAction);
+        Action actionEditProfile;
+        actionEditProfile = (Action) new UserActionFactory().createAction("editProfile");
+        Assert.assertTrue(actionEditProfile instanceof EditProfileAction);
     }
-        
+
     @Test
     public void createActionShowEditProfileTest() throws InvalidActionException {
 
-            Action actionShowEditProfile;
-            actionShowEditProfile = (Action) new UserActionFactory().createAction("showEditProfile");
-            Assert.assertTrue(actionShowEditProfile instanceof ShowEditProfileAction);
+        Action actionShowEditProfile;
+        actionShowEditProfile = (Action) new UserActionFactory().createAction("showEditProfile");
+        Assert.assertTrue(actionShowEditProfile instanceof ShowEditProfileAction);
     }
-            
+
     @Test
     public void createActionBanTest() throws InvalidActionException {
-            Action actionBan;
-            actionBan = (Action) new UserActionFactory().createAction("banUser");
-            Assert.assertTrue(actionBan instanceof BanAction);
+        Action actionBan;
+        actionBan = (Action) new UserActionFactory().createAction("banUser");
+        Assert.assertTrue(actionBan instanceof BanAction);
     }
-    
+
     @Test(expected = InvalidActionException.class)
-    public void createInvalidActionException() throws InvalidActionException{
+    public void createInvalidActionException() throws InvalidActionException {
         new UserActionFactory().createAction("Gianni");
     }
 }
