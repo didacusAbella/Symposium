3c4d3598e04ab1127303f1af21ce85e4dc9b7756
Angelo <maffettone@live.it>
2019-01-08 14:57:20 +0100

    Adding tests for AnswerModel. Fixing IntegrationTestSuite

--- src/test/java/it/blackhat/symposium/integration/IntegrationTestSuite.java
+++ src/test/java/it/blackhat/symposium/integration/IntegrationTestSuite.java
@@ -19,8 +19,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    AdminManagerTest.class,
-    UserModelManagerTest.class
 })
 public class IntegrationTestSuite {
     
