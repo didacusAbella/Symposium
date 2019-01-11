21d165aee7c36a6a04c41cc0b7cc924dfe8dc91b
Rayman <g.demaio24@studenti.unisa.it>
2019-01-11 12:02:33 +0100

    Ad report test finally

---
+++ src/test/java/it/blackhat/symposium/unit/ReportActionFactoryTest.java
@@ -0,0 +1,43 @@
+package it.blackhat.symposium.unit;
+
+import it.blackhat.symposium.actions.Action;
+import it.blackhat.symposium.actions.report.ReportActionFactory;
+import it.blackhat.symposium.actions.report.ReportQuestionAction;
+import it.blackhat.symposium.actions.report.ShowInsertReportAction;
+import it.blackhat.symposium.helpers.InvalidActionException;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Rayman
+ */
+
+public class ReportActionFactoryTest {
+
+    @Test
+    public void reportQuestionActionTest() throws InvalidActionException {
+
+
+        Action actionReportQuestion;
+        actionReportQuestion = new ReportActionFactory().createAction("insertReport");
+        Assert.assertTrue(actionReportQuestion instanceof ReportQuestionAction);
+
+
+    }
+
+    @Test
+    public void reportShowInsertReportActionTest() throws InvalidActionException {
+
+
+        Action actionReportShowInsert;
+        actionReportShowInsert = new ReportActionFactory().createAction("showInsertReport");
+        Assert.assertTrue(actionReportShowInsert instanceof ShowInsertReportAction);
+
+
+    }
+
+    @Test(expected = InvalidActionException.class)
+    public void createInvalidActionExceptionTest() throws InvalidActionException {
+        new ReportActionFactory().createAction("raiolaDoctor");
+    }
+}
