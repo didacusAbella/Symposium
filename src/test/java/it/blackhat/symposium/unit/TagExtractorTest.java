483d3868ecf356eac23878b5bb0926a634b873d7
Angelo <maffettone@live.it>
2019-01-11 10:56:30 +0100

    Fixing TagExtractorTest

--- src/test/java/it/blackhat/symposium/unit/TagExtractorTest.java
+++ src/test/java/it/blackhat/symposium/unit/TagExtractorTest.java
@@ -4,22 +4,21 @@ import it.blackhat.symposium.helpers.TagExtractor;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import javax.servlet.http.HttpServletRequest;
-public class TagExtractorTest {
 
+public class TagExtractorTest{
     @Test
-    public void tagExtractorTrue{
+    public void tagExtractorTrue(){
         HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
         Mockito.when(req.getParameter("tags")).thenReturn("ferrucci, esame, tsw");
         String[] tags = TagExtractor.extractTag(req);
         Assert.assertTrue(tags.length==3);
     }
     @Test
-    public void tagExtractorFalse{
+    public void tagExtractorFalse(){
         HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
-        Mockito.when(req.getParameter("tags")).thenReturn("ferrucci, esame, tsw");
+        Mockito.when(req.getParameter("tags")).thenReturn("ferrucci; esame; tsw");
         String[] tags = TagExtractor.extractTag(req);
-        Assert.assertFalse(tags.length==3);
+        Assert.assertTrue(tags.length==1);
     }
 }
