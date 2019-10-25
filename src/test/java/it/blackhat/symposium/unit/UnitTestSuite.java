0da69b089dfba80bfb014f188f5e44d999e77457
didacusabella <diequito93@gmail.com>
2019-10-25 15:52:51 +0200

    add filter tests

--- src/test/java/it/blackhat/symposium/unit/UnitTestSuite.java
+++ src/test/java/it/blackhat/symposium/unit/UnitTestSuite.java
@@ -2,6 +2,8 @@ package it.blackhat.symposium.unit;
 
 import it.blackhat.symposium.unit.helpers.*;
 import it.blackhat.symposium.unit.factories.*;
+import it.blackhat.symposium.unit.filters.AdminFilterTest;
+import it.blackhat.symposium.unit.filters.UserLoginFilterTest;
 import it.blackhat.symposium.unit.managers.*;
 import it.blackhat.symposium.unit.models.*;
 import org.junit.runner.RunWith;
@@ -33,7 +35,9 @@ import org.junit.runners.Suite;
   ReportModelTest.class,
   StatsModelTest.class,
   TagModelTest.class,
-  UserModelTest.class
+  UserModelTest.class,
+  AdminFilterTest.class,
+  UserLoginFilterTest.class
 })
 public class UnitTestSuite {
 
